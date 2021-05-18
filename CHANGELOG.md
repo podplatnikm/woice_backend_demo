a7d00187452a4ef1a3dbb69fb34312e8b1617160
Miha Podplatnik <miha.podplatnik@gmail.com>
2021-05-18 20:45:30 +0200

    Add: Verbose wide-appplication logging

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Changelog
+All notable changes to Porsche backend project will be documented in this file.
+
+## Unreleased
+### Added
+- Verbose logging
--- woice/settings/base.py
+++ woice/settings/base.py
@@ -127,3 +127,62 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Developers
 DEVELOPER_MAIL = "miha.podplatnik@gmail.com"
 ADMINS = (("Author", DEVELOPER_MAIL),)
+
+# Logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'django.server': {
+            '()': 'django.utils.log.ServerFormatter',
+            'format': '[%(server_time)s] %(message)s',
+        },
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'handlers': {
+        'django.server': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'django.server',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'propagate': True,
+        },
+        'django.server': {
+            'handlers': ['django.server'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+        'django.request': {
+            'handlers': ['mail_admins', 'console'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'INFO'
+        },
+    }
+}
