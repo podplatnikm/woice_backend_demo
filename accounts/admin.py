3278b18733ec2bc130e691dfb10d8f9f6bc781f7
Miha Podplatnik <miha.podplatnik@gmail.com>
2021-05-19 15:49:30 +0200

    Add: User is now required to pass email when creating in django admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,5 +5,8 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 @admin.register(get_user_model())
 class UserModelAdmin(BaseUserAdmin):
-    list_display = ("id", ) + BaseUserAdmin.list_display
+    list_display = ("id",) + BaseUserAdmin.list_display
     list_display_links = ("id", "username", "email")
+    add_fieldsets = (
+        (None, {"classes": ("wide",), "fields": ("email", "password1", "password2")}),
+    )
